486a20a3141abf4f9ffc9e1cf9c00967d226c5a5
danieldelimajj <111375723+danieldelimajj@users.noreply.github.com>
2025-07-25 02:20:08 -0300

    Primeiro commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+// Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TaskForm from "./components/TaskForm";
+
+
+// CSS
+import styles from "./App.module.css";
+import TaskList from "./components/TaskList";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+     <main className={styles.main}>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <TaskForm btnText="Criar Tarefa" />
+        </div>
+        <div>
+            <h2>Suas tarefas:</h2>
+            <TaskList />
+        </div>
+     </main>
+      <Footer />
     </div>
-  );
+  )
 }
 
 export default App;

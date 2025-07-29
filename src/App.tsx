793dae94572cbd7208d043aff1c8142626d00ac4
danieldelimajj <111375723+danieldelimajj@users.noreply.github.com>
2025-07-28 23:38:32 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Components
 import Header from "./components/Header";
@@ -11,14 +11,20 @@ import styles from "./App.module.css";
 import TaskList from "./components/TaskList";
 
 
+// Interface
+import { ITask } from "./interfaces/Task";
+
 function App() {
+   const [taskList, setTaskList] = useState<ITask[]>([]);
+     
+    
   return (
     <div>
       <Header />
      <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText="Criar Tarefa" />
+          <TaskForm btnText="Criar Tarefa" taskList={taskList} />
         </div>
         <div>
             <h2>Suas tarefas:</h2>
